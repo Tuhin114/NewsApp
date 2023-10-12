ca50b23bd2cc0b9d8ec33e2aceb0fff022010dcb
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-12 21:29:31 +0530

    Updated

--- src/components/News.js
+++ src/components/News.js
@@ -10,6 +10,7 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page:1
     };
   }
 
@@ -20,6 +21,40 @@ export class News extends Component {
     let parsedData = await data.json()
     console.log(parsedData);
     this.setState({articles: parsedData.articles})
+  }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=461d1ef454ad4675a25279810fdcfad4&page=${this.state.page - 1}`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles})
+
+
+    this.setState({
+      page : this.state.page - 1,
+      articles: parsedData.articles
+
+    })
+  }
+  
+  handleNextClick = async () => {
+    console.log("Next");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=461d1ef454ad4675a25279810fdcfad4&page=${this.state.page + 1}`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles})
+
+
+    this.setState({
+      page : this.state.page + 1,
+      articles: parsedData.articles
+
+    })
   }
 
   render() {
@@ -40,6 +75,10 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" class="btn btn-dark "onClick={this.handlePrevClick}> &larr; Previous</button>
+        <button type="button" class="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     );
