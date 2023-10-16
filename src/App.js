95dd1002f3e1ff0bf03332ebaf6e220ea8330a25
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-16 08:24:32 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,106 @@
-import './App.css';
+import "./App.css";
 
 //type rcc
-import React, { Component } from 'react'
-import Navbar from './components/Navbar';
-import News from './components/News';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize={9} country="in" category="sports"/>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <News
+                  key="general"
+                  pageSize={12}
+                  country="us"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pageSize={12}
+                  country="us"
+                  category="business"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pageSize={12}
+                  country="us"
+                  category="entertainment"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/health"
+              element={
+                <News
+                  key="health"
+                  pageSize={12}
+                  country="us"
+                  category="health"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/science"
+              element={
+                <News
+                  key="science"
+                  pageSize={12}
+                  country="us"
+                  category="science"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={12}
+                  country="us"
+                  category="sports"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News
+                  key="technology"
+                  pageSize={12}
+                  country="us"
+                  category="technology"
+                />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
       </div>
-    )
+    );
   }
 }
-
